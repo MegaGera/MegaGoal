92fb33bf50d9c2999de2c59d65f7e6220b1dc0ae
MegaGera <gera1397@gmail.com>
2024-09-10 01:12:30 +0100

    Change images references to megamedia

--- WebApp/src/app/services/images.service.ts
+++ WebApp/src/app/services/images.service.ts
@@ -2,11 +2,17 @@
   Image service for retrieve the images routes from the assets folder
 */
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImagesService {
+
+  /*
+    URL of the API server
+  */
+  url = environment.serverImagesURL;
 
   constructor() { }
     
@@ -14,28 +20,29 @@ export class ImagesService {
     Get the route for a league image
   */
   getRouteImageLeague(id: number | string): string {
-    return "/assets/img/leagues/" + id + ".png"; 
+    return this.url + "/leagues/" + id + ".png"; 
   }
 
   /*
     Get the route for a league image back
   */
   getRouteImageLeagueBack(id: number | string): string {
-    return "/assets/img/leagues/back/" + id + ".png"; 
+    return this.url + "/leagues/back/" + id + ".png"; 
   }
 
   /*
     Get the route for a league image back
   */
   getRouteImageLeagueSm(id: number | string): string {
-    return "/assets/img/leagues/sm/" + id + ".png"; 
+    return this.url + "/leagues/sm/" + id + ".png"; 
+
   }
 
   /*
     Get the route for a team image
   */
   getRouteImageTeam(id: number | string): string {
-    return "/assets/img/teams/team_" + id + ".png"; 
+    return this.url + "/teams/team_" + id + ".png"; 
   }
 
 }
