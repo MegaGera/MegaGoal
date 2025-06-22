5d09e5ca5345de9437a609aafca929d0711be7e7
MegaGera <gonzalo.berne@gmail.com>
2025-06-22 23:19:14 +0100

    Top Menu styles updated

--- WebApp/src/app/components/top-menu/top-menu.component.ts
+++ WebApp/src/app/components/top-menu/top-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NgIconComponent, provideIcons, provideNgIconsConfig } from '@ng-icons/core';
 import { jamUserCircle, jamEyeF } from '@ng-icons/jam-icons';
@@ -10,9 +10,21 @@ import { jamUserCircle, jamEyeF } from '@ng-icons/jam-icons';
   templateUrl: './top-menu.component.html',
   styleUrl: './top-menu.component.css',
   providers: [provideNgIconsConfig({
-    size: '3em',
+    size: '2.5em',
   }), provideIcons({ jamUserCircle, jamEyeF })]
 })
 export class TopMenuComponent {
+  isUserMenuOpen = false;
 
+  toggleUserMenu(): void {
+    this.isUserMenuOpen = !this.isUserMenuOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event): void {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.user-dropdown')) {
+      this.isUserMenuOpen = false;
+    }
+  }
 }
