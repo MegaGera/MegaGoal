f5d87df8fb22fba346550ab40ba067aeb0b06905
MegaGera <gonzalo.berne@gmail.com>
2025-08-12 01:04:21 +0100

    Settings changed to league_settings

--- Server/controllers/adminController.js
+++ Server/controllers/adminController.js
@@ -4,7 +4,7 @@ import { getDB } from '../config/db.js';
 export const getLeaguesSettings = async (req, res) => {
   const db = getDB();
   try {
-    const result = await db.collection('settings').find().toArray();
+    const result = await db.collection('league_settings').find().toArray();
     console.log("Leagues Settings Getted");
     res.send(result);
   } catch (error) {
@@ -19,7 +19,7 @@ export const changeIsActive = async (req, res) => {
     let {league_id, is_active} = req.body;
     const filter = { "league_id": +league_id};
     const update = is_active ? { $set: { "is_active": is_active } } : { $set: { "is_active": is_active, "daily_update": false } };
-    let result = await db.collection('settings').updateOne(filter, update);
+    let result = await db.collection('league_settings').updateOne(filter, update);
     console.log("Update is_active for league " + league_id + " to " + is_active);
     res.status(201).json(result.acknowledged);
   } catch (error) {
@@ -34,7 +34,7 @@ export const changeUpdateFrequency = async (req, res) => {
     let {league_id, update_frequency} = req.body;
     const filter = { "league_id": +league_id};
     const update = { $set: { "update_frequency": update_frequency } };
-    let result = await db.collection('settings').updateOne(filter, update);
+    let result = await db.collection('league_settings').updateOne(filter, update);
     console.log("Update frequency updated for league " + league_id + " to " + update_frequency);
     res.status(201).json(result.acknowledged);
   } catch (error) {
@@ -49,7 +49,7 @@ export const changeDailyUpdate = async (req, res) => {
     let {league_id, daily_update} = req.body;
     const filter = { "league_id": +league_id, "is_active": true };
     const update = { $set: { "daily_update": daily_update } };
-    let result = await db.collection('settings').updateOne(filter, update);
+    let result = await db.collection('league_settings').updateOne(filter, update);
     console.log("Update daily_update for league " + league_id + " to " + daily_update);
     res.status(201).json(result.acknowledged);
   } catch (error) {
