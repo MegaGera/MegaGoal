a26ceb38acd6da8b724dc62c8b1cad80452f60da
MegaGera <gonzalo.berne@gmail.com>
2025-09-22 02:22:57 +0100

    FIxed url error

--- WebApp/src/app/services/landing.service.ts
+++ WebApp/src/app/services/landing.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Match } from '../models/match';
+import { environment } from '../../environments/environment';
 
 export interface LandingPageInfo {
   matches: Match[];
@@ -11,11 +12,11 @@ export interface LandingPageInfo {
   providedIn: 'root'
 })
 export class LandingService {
-  private apiUrl = 'http://localhost:3150/public/match'; // Public endpoint
+  private apiUrl = environment.serverURL;
 
   constructor(private http: HttpClient) { }
 
   getLandingPageInfo(): Observable<LandingPageInfo> {
-    return this.http.get<LandingPageInfo>(`${this.apiUrl}/landing-info`);
+    return this.http.get<LandingPageInfo>(`${this.apiUrl}/public/match/landing-info`);
   }
 }
