ef30a62dea447de9c9318d179cc00bfc071407d5
MegaGera <gonzalo.berne@gmail.com>
2025-12-02 22:03:37 +0000

    Update all leagues everyday with a cron

---
+++ Updater/leagues_updater.py
@@ -0,0 +1,16 @@
+import datetime
+from utils import MatchUpdater
+
+def perform_leagues_update():
+    """Perform leagues update operation"""
+    print("Starting leagues_updater.py")
+    print(f"-------------- Starting New Update On --------------")
+    print(datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
+    
+    updater = MatchUpdater()
+    updater.add_leagues()
+    print("Leagues update completed successfully")
+
+if __name__ == "__main__":
+    perform_leagues_update()
+
